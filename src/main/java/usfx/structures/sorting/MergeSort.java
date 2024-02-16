fada2444ce866b3aae803a65a113ab602ef62c15
llanosmjla <llanos.mendoza.joseluisalberto@usfx.bo>
2024-02-16 18:25:53 -0400

    Incremental project completedd

---
+++ src/main/java/usfx/structures/sorting/MergeSort.java
@@ -0,0 +1,54 @@
+package usfx.structures.sorting;
+
+public class MergeSort {
+    public static void mergeSort(int[] array) {
+        if (array.length > 1) {
+            int mid = array.length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[array.length - mid];
+            for (int i = 0; i < mid; i++) {
+                left[i] = array[i];
+            }
+            for (int i = mid; i < array.length; i++) {
+                right[i - mid] = array[i];
+            }
+            mergeSort(left);
+            mergeSort(right);
+            merge(array, left, right);
+        }
+    }
+
+    public static void merge(int[] array, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k] = left[i];
+                i++;
+            } else {
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            array[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            array[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 3, 8, 6, 2, 7, 1, 4};
+        mergeSort(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
